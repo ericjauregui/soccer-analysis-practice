d718cd1f2c1c97f3cb50c851083e4b2fb266c2a2
ERIC JAUREGUI <eric.jauregui@ymail.com>
2025-03-06 11:33:40 -0800

    pushing all changes to main

--- code/03_04_granularity.py
+++ code/03_04_granularity.py
@@ -5,38 +5,38 @@ from os import path
 # stored
 # on Windows it might be something like 'C:/mydir'
 
-DATA_DIR = './data'
+DATA_DIR = "./data"
 
-shots = pd.read_csv(path.join(DATA_DIR, 'shots.csv'))  # shot data
+shots = pd.read_csv(path.join(DATA_DIR, "shots.csv"))  # shot data
 
 # Granularity
 
 # Grouping
 # TODO: make shot distance from this
-shots.groupby('match_id').sum().head()
+shots.groupby("match_id").sum().head()
 
-shots['attempt'] = 1
-sum_cols = ['goal', 'attempt', 'accurate', 'counter', 'opportunity']
-shots.groupby('match_id').sum()[sum_cols].head()
+shots["attempt"] = 1
+sum_cols = ["goal", "attempt", "accurate", "counter", "opportunity"]
+shots.groupby("match_id").sum()[sum_cols].head()
 
-shots.groupby('match_id').agg({
-    'goal': 'sum',
-    'attempt': 'count',
-    'dist_m': 'mean',
-    'dist_ft': 'mean'}).head()
+shots.groupby("match_id").agg(
+    {"goal": "sum", "attempt": "count", "dist_m": "mean", "dist_ft": "mean"}
+).head()
 
-shots.groupby('match_id').agg(
-    goal = ('goal', 'sum'),
-    attempt = ('attempt', 'count'),
-    ave_dist_m = ('dist_m', 'mean'),
-    ave_dist_ft = ('dist_ft', 'mean')).head()
+shots.groupby("match_id").agg(
+    goal=("goal", "sum"),
+    attempt=("attempt", "count"),
+    ave_dist_m=("dist_m", "mean"),
+    ave_dist_ft=("dist_ft", "mean"),
+).head()
 
-shots_team = shots.groupby(['match_id', 'team_id']).agg(
-    goal = ('goal', 'sum'),
-    attempt = ('attempt', 'count'),
-    ave_dist_m = ('dist_m', 'mean'),
-    min_dist_m = ('dist_m', 'min'),
-    max_dist_ft = ('dist_ft', 'max'))
+shots_team = shots.groupby(["match_id", "team_id"]).agg(
+    goal=("goal", "sum"),
+    attempt=("attempt", "count"),
+    ave_dist_m=("dist_m", "mean"),
+    min_dist_m=("dist_m", "min"),
+    max_dist_ft=("dist_ft", "max"),
+)
 
 shots_team.head()
 
@@ -44,15 +44,19 @@ shots_team.head()
 shots_team.loc[[(2057954, 14358), (2058017, 4418)]]
 
 # Stacking and unstacking data
-fd = shots.query("foot in ('left', 'right')").groupby(
-    ['name', 'foot'])['dist_m'].mean().reset_index()
+fd = (
+    shots.query("foot in ('left', 'right')")
+    .groupby(["name", "foot"])["dist_m"]
+    .mean()
+    .reset_index()
+)
 fd.head()
 
-fd_reshaped = fd.set_index(['name', 'foot']).unstack()
+fd_reshaped = fd.set_index(["name", "foot"]).unstack()
 fd_reshaped.head()
 
-fd_reshaped.columns = ['left', 'right']
-(fd_reshaped['right'] - fd_reshaped['left']).mean()
+fd_reshaped.columns = ["left", "right"]
+(fd_reshaped["right"] - fd_reshaped["left"]).mean()
 
 fd_reshaped.idxmax(axis=1).value_counts()
 
--- solutions-to-exercises/03_pandas_answers.py
+++ solutions-to-exercises/03_pandas_answers.py
@@ -7,50 +7,52 @@ Questions with written (not code) answers are inside triple quotes.
 # PANDAS BASICS
 ###############################################################################
 
+from os import path
+
+import numpy as np
+import pandas as pd
+
 #######
 # 3.0.1
 #######
-import pandas as pd
-from os import path
 
-DATA_DIR = './data'
-dfm = pd.read_csv(path.join(DATA_DIR, 'matches.csv'))
+DATA_DIR = "./data"
+dfm = pd.read_csv(path.join(DATA_DIR, "matches.csv"))
 
 #######
 # 3.0.2
 #######
-dfm10 = dfm.sort_values('date').head(10)
+dfm10 = dfm.sort_values("date").head(10)
 
 #######
 # 3.0.3
 #######
-dfm.sort_values('label', ascending=False, inplace=True)
+dfm.sort_values("label", ascending=False, inplace=True)
 dfm.head()
-
-# Note: if this didn't work when you printed it on a new line in the REPL you
-# probably forgot the `inplace=True` argument.
 
 #######
 # 3.0.4
 #######
-type(dfm.sort_values('label'))  # it's a DataFrame
+type(dfm.sort_values("label"))  # it's a DataFrame
 
 #######
 # 3.0.5
 #######
 # a
-match_simple = dfm[['date', 'home_team', 'away_team', 'home_score',
-    'away_score']]
+match_simple = dfm[
+    ["date", "home_team", "away_team", "home_score", "away_score"]
+]
 
 # b
-match_simple = match_simple[['home_team', 'away_team', 'date', 'home_score',
-    'away_score']]
+match_simple = match_simple[
+    ["home_team", "away_team", "date", "home_score", "away_score"]
+]
 
 # c
-match_simple['match_id'] = dfm['match_id']
+match_simple["match_id"] = dfm["match_id"]
 
 # d
-dfm.to_csv(path.join(DATA_DIR, 'match_simple.txt'), sep='|')
+dfm.to_csv(path.join(DATA_DIR, "match_simple.txt"), sep="|")
 
 ###############################################################################
 # COLUMNS
@@ -59,59 +61,56 @@ dfm.to_csv(path.join(DATA_DIR, 'match_simple.txt'), sep='|')
 #######
 # 3.1.1
 #######
-import pandas as pd
-from os import path
 
-DATA_DIR = './data'
-pm = pd.read_csv(path.join(DATA_DIR, 'player_match.csv'))
+DATA_DIR = "./data"
+pm = pd.read_csv(path.join(DATA_DIR, "player_match.csv"))
 
 #######
 # 3.1.2
 #######
-pm['ob_touches'] = pm['throw'] + pm['corner']
-pm['ob_touches'].head()
+pm["ob_touches"] = pm["throw"] + pm["corner"]
+pm["ob_touches"].head()
 
 #######
 # 3.1.3
 #######
-pm['player_desc'] = pm['name'] + ' is the ' + pm['team'] + ' ' + pm['pos']
-pm['player_desc'].head()
+pm["player_desc"] = pm["name"] + " is the " + pm["team"] + " " + pm["pos"]
+pm["player_desc"].head()
 
 #######
 # 3.1.4
 #######
-pm['at_least_one_throwin'] = pm['throw'] > 0
-pm['at_least_one_throwin'].head()
+pm["at_least_one_throwin"] = pm["throw"] > 0
+pm["at_least_one_throwin"].head()
 
 #######
 # 3.1.5
 #######
-pm['len_last_name'] = (pm['name']
-                       .apply(lambda x: len(x.split('.')[-1])))
-pm['len_last_name'].head()
+pm["len_last_name"] = pm["name"].apply(lambda x: len(x.split(".")[-1]))
+pm["len_last_name"].head()
 
 #######
 # 3.1.6
 #######
-pm['match_id'] = pm['match_id'].astype(str)
+pm["match_id"] = pm["match_id"].astype(str)
 
 #######
 # 3.1.7
 #######
 # a
-pm.columns = [x.replace('_', ' ') for x in pm.columns]
+pm.columns = [x.replace("_", " ") for x in pm.columns]
 pm.head()
 
 # b
-pm.columns = [x.replace(' ', '_') for x in pm.columns]
+pm.columns = [x.replace(" ", "_") for x in pm.columns]
 pm.head()
 
 #######
 # 3.1.8
 #######
 # a
-pm['air_duel_won_percentage'] = pm['air_duel_won']/pm['air_duel']
-pm['air_duel_won_percentage'].head()
+pm["air_duel_won_percentage"] = pm["air_duel_won"] / pm["air_duel"]
+pm["air_duel_won_percentage"].head()
 
 # b
 """
@@ -121,13 +120,13 @@ had 0 rebounds.
 """
 
 # To replace all the missing values with `-99`:
-pm['air_duel_won_percentage'].fillna(-99, inplace=True)
-pm['air_duel_won_percentage'].head()
+pm["air_duel_won_percentage"].fillna(-99, inplace=True)
+pm["air_duel_won_percentage"].head()
 
 #######
 # 3.1.9
 #######
-pm.drop('air_duel_won_percentage', axis=1, inplace=True)
+pm.drop("air_duel_won_percentage", axis=1, inplace=True)
 pm.head()
 
 # If you forget the `axis=1` Pandas will try to drop the *row* with the
@@ -147,43 +146,42 @@ pm.head()
 #######
 # 3.2.1
 #######
-import pandas as pd
-from os import path
-
-DATA_DIR = './data'
-pm = pd.read_csv(path.join(DATA_DIR, 'player_match.csv'))
+DATA_DIR = "./data"
+pm = pd.read_csv(path.join(DATA_DIR, "player_match.csv"))
 
 #######
 # 3.2.2
 #######
-pm['named_pass1'] = pm['clearance'] + pm['cross'] + pm['assist'] + pm['keypass']
+pm["named_pass1"] = (
+    pm["clearance"] + pm["cross"] + pm["assist"] + pm["keypass"]
+)
 
-pm['named_pass2'] = pm[['clearance', 'cross', 'assist', 'keypass']].sum(axis=1)
+pm["named_pass2"] = pm[["clearance", "cross", "assist", "keypass"]].sum(axis=1)
 
-(pm['named_pass1'] == pm['named_pass2']).all()
+(pm["named_pass1"] == pm["named_pass2"]).all()
 
 #######
 # 3.2.3
 #######
 
 # a
-pm[['shot', 'assist', 'pass']].mean()
+pm[["shot", "assist", "pass"]].mean()
 
 # shot       0.817475
 # assist     0.050269
 # pass      31.599641
 
 # b
-((pm['goal'] >= 1) & (pm['assist'] >= 1)).sum()  # 10
+((pm["goal"] >= 1) & (pm["assist"] >= 1)).sum()  # 10
 
 # c
-((pm['goal'] >= 1) & (pm['assist'] >= 1)).sum()/(pm.shape[0])  # 0.5984%
+((pm["goal"] >= 1) & (pm["assist"] >= 1)).sum() / (pm.shape[0])  # 0.5984%
 
 # d
-pm['own_goal'].sum()  # 10
+pm["own_goal"].sum()  # 10
 
 # e
-pm['pos'].value_counts()  # MID - 636
+pm["pos"].value_counts()  # MID - 636
 
 ###############################################################################
 # FILTERING
@@ -191,31 +189,32 @@ pm['pos'].value_counts()  # MID - 636
 #######
 # 3.3.1
 #######
-import pandas as pd
-from os import path
-
-DATA_DIR = './data'
-dfp = pd.read_csv(path.join(DATA_DIR, 'players.csv'))
+DATA_DIR = "./data"
+dfp = pd.read_csv(path.join(DATA_DIR, "players.csv"))
 
 #######
 # 3.3.2
 #######
 # a
-dfp_bra1 = dfp.loc[dfp['team'] == 'Brazil', ['player_name', 'pos', 'foot',
-    'weight', 'height']]
+dfp_bra1 = dfp.loc[
+    dfp["team"] == "Brazil", ["player_name", "pos", "foot", "weight", "height"]
+]
 
 dfp_bra1.head()
 
 # b
-dfp_bra2 = dfp.query("team == 'Brazil'")[['player_name', 'pos', 'foot',
-    'weight', 'height']]
+dfp_bra2 = dfp.query("team == 'Brazil'")[
+    ["player_name", "pos", "foot", "weight", "height"]
+]
 dfp_bra2.head()
 
 #######
 # 3.3.3
 #######
-dfp_no_bra = dfp.loc[dfp['team'] != 'Brazil', ['team', 'player_name', 'pos',
-    'foot', 'weight', 'height']]
+dfp_no_bra = dfp.loc[
+    dfp["team"] != "Brazil",
+    ["team", "player_name", "pos", "foot", "weight", "height"],
+]
 dfp_no_bra.head()
 
 #######
@@ -223,12 +222,12 @@ dfp_no_bra.head()
 #######
 
 # a
-dfp['bday'] = dfp['birth_date'].astype(str).str[-4:]
-dfp['bday'].duplicated().any()  # yes there are
+dfp["bday"] = dfp["birth_date"].astype(str).str[-4:]
+dfp["bday"].duplicated().any()  # yes there are
 
 # b
 # flags ALL dups (not just 2nd) because passing keep=False
-dups = dfp[['bday']].duplicated(keep=False)
+dups = dfp[["bday"]].duplicated(keep=False)
 
 dfp_dups = dfp.loc[dups]
 dfp_no_dups = dfp.loc[~dups]
@@ -236,18 +235,17 @@ dfp_no_dups = dfp.loc[~dups]
 #######
 # 3.3.5
 #######
-import numpy as np
 
-dfp['height_description'] = np.nan
-dfp.loc[dfp['height'] > 190, 'height_description'] = 'tall'
-dfp.loc[dfp['height'] < 170, 'height_description'] = 'short'
-dfp[['height', 'height_description']].sample(5)
+dfp["height_description"] = np.nan
+dfp.loc[dfp["height"] > 190, "height_description"] = "tall"
+dfp.loc[dfp["height"] < 170, "height_description"] = "short"
+dfp[["height", "height_description"]].sample(5)
 
 #######
 # 3.3.6
 #######
 # a
-dfp_no_desc1 = dfp.loc[dfp['height_description'].isnull()]
+dfp_no_desc1 = dfp.loc[dfp["height_description"].isnull()]
 
 # b
 dfp_no_desc2 = dfp.query("height_description.isnull()")
@@ -271,29 +269,28 @@ information.
 #######
 
 # a
-import pandas as pd
-from os import path
 
-DATA_DIR = './data'
-dfpm = pd.read_csv(path.join(DATA_DIR, 'player_match.csv'))
+DATA_DIR = "./data"
+dfpm = pd.read_csv(path.join(DATA_DIR, "player_match.csv"))
 
 # b
-dfpm.groupby('player_id')['shot', 'goal'].mean()
+dfpm.groupby("player_id")["shot", "goal"].mean()
 
 # c
-player_ave = dfpm.groupby('player_id')['shot', 'goal'].mean()
-(player_ave['shot'] >= 4).mean()  # 1.05%
+player_ave = dfpm.groupby("player_id")["shot", "goal"].mean()
+(player_ave["shot"] >= 4).mean()  # 1.05%
 
 #######
 # 3.4.3
 #######
 
 # a
-dftm = dfpm.groupby(['match_id', 'team']).agg(
-    total_goal = ('goal', 'sum'),
-    total_pass = ('pass', 'sum'),
-    total_shot = ('shot', 'sum'),
-    nplayed = ('player_id', 'count'))
+dftm = dfpm.groupby(["match_id", "team"]).agg(
+    total_goal=("goal", "sum"),
+    total_pass=("pass", "sum"),
+    total_shot=("shot", "sum"),
+    nplayed=("player_id", "count"),
+)
 
 dftm.head()
 
@@ -301,12 +298,12 @@ dftm.head()
 dftm.reset_index(inplace=True)
 
 # c
-dftm['no_goals'] = dftm['total_goal'] == 0
-dftm.groupby('no_goals')['total_pass', 'total_shot'].mean()
+dftm["no_goals"] = dftm["total_goal"] == 0
+dftm.groupby("no_goals")["total_pass", "total_shot"].mean()
 
 # d
-dftm.groupby('team')['match_id'].count()
-dfpm.groupby('team')['match_id'].sum()
+dftm.groupby("team")["match_id"].count()
+dfpm.groupby("team")["match_id"].sum()
 
 """
 Count counts the number of non missing (non `np.nan`) values. This is different
@@ -331,28 +328,30 @@ information.
 # 3.5.1
 #######
 # a
-import pandas as pd
-from os import path
-
-DATA_DIR = './data'
-df_name = pd.read_csv(path.join(DATA_DIR, 'problems/combine1', 'name.csv'))
-df_shot = pd.read_csv(path.join(DATA_DIR, 'problems/combine1', 'shot.csv'))
-df_pass = pd.read_csv(path.join(DATA_DIR, 'problems/combine1', 'pass.csv'))
-df_ob = pd.read_csv(path.join(DATA_DIR, 'problems/combine1', 'ob.csv'))
+DATA_DIR = "./data"
+df_name = pd.read_csv(path.join(DATA_DIR, "problems/combine1", "name.csv"))
+df_shot = pd.read_csv(path.join(DATA_DIR, "problems/combine1", "shot.csv"))
+df_pass = pd.read_csv(path.join(DATA_DIR, "problems/combine1", "pass.csv"))
+df_ob = pd.read_csv(path.join(DATA_DIR, "problems/combine1", "ob.csv"))
 
 # b
-df_comb1 = pd.merge(df_name, df_shot, how='left')
-df_comb1 = pd.merge(df_comb1, df_pass, how='left')
-df_comb1 = pd.merge(df_comb1, df_ob, how='left')
+df_comb1 = pd.merge(df_name, df_shot, how="left")
+df_comb1 = pd.merge(df_comb1, df_pass, how="left")
+df_comb1 = pd.merge(df_comb1, df_ob, how="left")
 
 df_comb1 = df_comb1.fillna(0)
 
 # c
-df_comb2 = pd.concat([df_name.set_index(['player_id', 'match_id']),
-                      df_shot.set_index(['player_id', 'match_id']),
-                      df_pass.set_index(['player_id', 'match_id']),
-                      df_ob.set_index(['player_id', 'match_id'])], join='outer',
-                     axis=1)
+df_comb2 = pd.concat(
+    [
+        df_name.set_index(["player_id", "match_id"]),
+        df_shot.set_index(["player_id", "match_id"]),
+        df_pass.set_index(["player_id", "match_id"]),
+        df_ob.set_index(["player_id", "match_id"]),
+    ],
+    join="outer",
+    axis=1,
+)
 
 df_comb2 = df_comb2.fillna(0)
 
@@ -371,13 +370,11 @@ concating.
 ########
 # 3.5.2a
 ########
-import pandas as pd
-from os import path
 
-DATA_DIR = './data'
-df_d = pd.read_csv(path.join(DATA_DIR, 'problems/combine2', 'def.csv'))
-df_f = pd.read_csv(path.join(DATA_DIR, 'problems/combine2', 'fwd.csv'))
-df_m = pd.read_csv(path.join(DATA_DIR, 'problems/combine2', 'mid.csv'))
+DATA_DIR = "./data"
+df_d = pd.read_csv(path.join(DATA_DIR, "problems/combine2", "def.csv"))
+df_f = pd.read_csv(path.join(DATA_DIR, "problems/combine2", "fwd.csv"))
+df_m = pd.read_csv(path.join(DATA_DIR, "problems/combine2", "mid.csv"))
 
 # b
 df = pd.concat([df_d, df_f, df_m], ignore_index=True)
@@ -386,19 +383,23 @@ df = pd.concat([df_d, df_f, df_m], ignore_index=True)
 # 3.5.3
 #######
 # a
-import pandas as pd
-from os import path
 
-DATA_DIR = './data'
-dft = pd.read_csv(path.join(DATA_DIR, 'teams.csv'))
+DATA_DIR = "./data"
+dft = pd.read_csv(path.join(DATA_DIR, "teams.csv"))
 
 # b
-for group in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']:
-    (dft
-        .query(f"grouping == '{group}'")
-        .to_csv(path.join(DATA_DIR, f'dft_{group}.csv'), index=False))
+for group in ["A", "B", "C", "D", "E", "F", "G", "H"]:
+    (
+        dft.query(f"grouping == '{group}'").to_csv(
+            path.join(DATA_DIR, f"dft_{group}.csv"), index=False
+        )
+    )
 
 # c
-df = pd.concat([pd.read_csv(path.join(DATA_DIR, f'dft_{group}.csv'))
-    for group in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']], ignore_index=True)
-
+df = pd.concat(
+    [
+        pd.read_csv(path.join(DATA_DIR, f"dft_{group}.csv"))
+        for group in ["A", "B", "C", "D", "E", "F", "G", "H"]
+    ],
+    ignore_index=True,
+)
